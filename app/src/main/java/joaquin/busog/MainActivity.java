9ae3d6295a754d64f942bcf9c89b20f81ee13ce9
Albdog <joaquinjacinto11@gmail.com>
2017-10-22 23:53:49 +0800

    Created main menu, restaurant menu, and added databases

--- app/src/main/java/joaquin/busog/MainActivity.java
+++ app/src/main/java/joaquin/busog/MainActivity.java
@@ -1,7 +1,10 @@
 package joaquin.busog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mButton = (Button) findViewById(R.id.restaurantsButton);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Context context = getApplicationContext();
+//                CharSequence text = "Hello toast!";
+//                int duration = Toast.LENGTH_SHORT;
+//
+//                Toast toast = Toast.makeText(context, text, duration);
+//                toast.show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+//    @OnClick (R.id.restaurantsButton)
+//    public void startRestaurantActivity(View view) {
+//        Context context = getApplicationContext();
+//        CharSequence text = "Hello toast!";
+//        int duration = Toast.LENGTH_SHORT;
+//
+//        Toast toast = Toast.makeText(context, text, duration);
+//        toast.show();
+//        Intent intent = new Intent(this, RestaurantsActivity.class);
+//        startActivity(intent);
+//    }
 }
