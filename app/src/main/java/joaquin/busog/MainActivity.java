b85f0ffed5eb33318529b8a3ec44835d473136af
Albdog <joaquinjacinto11@gmail.com>
2017-11-01 00:15:54 +0800

    Revert to initial commit

--- app/src/main/java/joaquin/busog/MainActivity.java
+++ app/src/main/java/joaquin/busog/MainActivity.java
@@ -1,44 +1,12 @@
 package joaquin.busog;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button mButton = (Button) findViewById(R.id.restaurantsButton);
-
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Context context = getApplicationContext();
-//                CharSequence text = "Hello toast!";
-//                int duration = Toast.LENGTH_SHORT;
-//
-//                Toast toast = Toast.makeText(context, text, duration);
-//                toast.show();
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                startActivity(intent);
-            }
-        });
     }
-
-//    @OnClick (R.id.restaurantsButton)
-//    public void startRestaurantActivity(View view) {
-//        Context context = getApplicationContext();
-//        CharSequence text = "Hello toast!";
-//        int duration = Toast.LENGTH_SHORT;
-//
-//        Toast toast = Toast.makeText(context, text, duration);
-//        toast.show();
-//        Intent intent = new Intent(this, RestaurantsActivity.class);
-//        startActivity(intent);
-//    }
 }
