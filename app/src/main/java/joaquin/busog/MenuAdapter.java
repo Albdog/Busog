781dedb07c57c0cc2a0f400da75ee633b40b8ba0
Albdog <joaquinjacinto11@gmail.com>
2017-11-02 01:11:07 +0800

    Added menu adapter

--- app/src/main/java/joaquin/busog/MenuAdapter.java
+++ app/src/main/java/joaquin/busog/MenuAdapter.java
@@ -7,17 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MenuAdapter extends BaseAdapter {
 
     private Context mContext;
-    private String[] mRestaurant;
+    private String[] mMenuItems;
 
-    public MenuAdapter(Context context, String[] restaurant) {
+    public MenuAdapter(Context context, String[] menuItems) {
         mContext = context;
-        mRestaurant = restaurant;
+        mMenuItems = menuItems;
     }
 
     @Override
@@ -37,15 +38,20 @@ public class MenuAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        final ViewThing thing;
+        final MenuView menuView;
 
         if(view == null) {
             view = LayoutInflater.from(mContext).inflate(R.layout.menu_item, null);
-            thing = new ViewThing();
-            thing.restaurantName = (TextView) view.findViewById(R.id.restaurantName);
+            menuView = new MenuView();
+            menuView.itemImage = view.findViewById(R.id.itemImage);
+            menuView.itemName = view.findViewById(R.id.itemName);
+            menuView.alaCartePrice = view.findViewById(R.id.alaCartePrice);
+            menuView.smallPrice = view.findViewById(R.id.smallPrice);
+            menuView.mediumPrice = view.findViewById(R.id.mediumPrice);
+            menuView.largePrice = view.findViewById(R.id.largePrice);
 
             final View finalView = view;
-            thing.restaurantName.setOnClickListener(new View.OnClickListener() {
+            menuView.itemName.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(finalView.getContext(), "tests", Toast.LENGTH_SHORT).show();
@@ -56,21 +62,22 @@ public class MenuAdapter extends BaseAdapter {
             });
         }
         else {
-            thing = (ViewThing) view.getTag();
+            menuView = (MenuView) view.getTag();
         }
 
-        updateUI(i, thing);
+        updateUI(i, menuView);
 
         return view;
     }
 
-    private void updateUI(int i, ViewThing thing) {
-        String name = mRestaurant[i];
+    private void updateUI(int i, MenuView menuView) {
+        String name = mMenuItems[i];
 
-        thing.restaurantName.setText(name);
+        menuView.itemName.setText(name);
     }
 
-    private static class ViewThing extends AppCompatActivity {
-        TextView restaurantName;
+    private static class MenuView extends AppCompatActivity {
+        ImageView itemImage;
+        TextView itemName, alaCartePrice, smallPrice, mediumPrice, largePrice;
     }
 }
