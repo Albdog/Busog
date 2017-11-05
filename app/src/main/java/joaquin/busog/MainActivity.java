dd4ddb80e77c87801b2cc34d67d1ee21592803f3
Albdog <joaquinjacinto11@gmail.com>
2017-11-05 22:14:55 +0800

    Fixed text sizes

--- app/src/main/java/joaquin/busog/MainActivity.java
+++ app/src/main/java/joaquin/busog/MainActivity.java
@@ -3,11 +3,7 @@ package joaquin.busog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -18,12 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-    }
-
-    @OnClick (R.id.button)
-    public void test() {
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
     }
 
     @OnClick (R.id.button2)
